3309c72e7ea640c03da00b5b7e439bb774453f3e
Paul ETIENNE <paul.etienne@mines-paristech.fr>
2022-11-09 16:58:10 +0100

    Sixième commit : ajout fonction de sauvegarde de partie

--- snake.py
+++ snake.py
@@ -4,15 +4,6 @@ import sys
 
 
 pg.init()
-screen = pg.display.set_mode((600, 600))
-clock = pg.time.Clock()
-
-#liste des coordonnées pour tracer le damier en arrière-plan
-checker_index = []
-for i in range(30):
-    for j in range(30):
-        if (i + j) % 2 == 0:
-            checker_index.append([i, j])
 
 #tailles des carrés
 width = 20
@@ -28,10 +19,17 @@ direction = [1, 0]
 fruit = [10, 10]
 score = 0
 
-# Sauvegarde de partie
-save = open("save.txt", "r")
+#liste des coordonnées pour tracer le damier en arrière-plan
+checker_index = []
+for i in range(30):
+    for j in range(30):
+        if (i + j) % 2 == 0:
+            checker_index.append([i, j])
 
-# Définition d'une fonction annexe pour le bot
+running = True # True tant que le programme tourne
+bot = False # True quand le bot est activé
+
+# Définition de fonctions annexe pour le bot 
 def signe(x):
     '''Renvoie -1 si x négatif et +1 si x positif'''
     if x >= 0:
@@ -65,13 +63,39 @@ def dir_bot(serpent, direction, fruit):
         return dir1
     else:
         return dir2
-    
+# Fin de la définition de fonctions annexe pour le bot
 
-# on rajoute une condition à la boucle: si on la passe à False le programme s'arrête
-running = True
-bot = False
-while running:
-    clock.tick(3)
+# Définition fonction de sauvegarde
+def save_state(serpent, direction, fruit, score):
+    save = open("save.py", "w")
+    vals = [serpent, direction, fruit, score]
+    names = ["save_serpent", "save_direction", "save_fruit", "save_score"]
+    txt = ""
+    for k in range(4):
+        txt = txt + names[k] + f" = {vals[k]} ; "
+    save.write(txt)
+
+
+# Définition fonction chargement de sauvegarde
+def load_state():
+    global serpent, direction, fruit, score
+    from save import save_direction, save_fruit, save_score, save_serpent
+    serpent = save_serpent
+    direction = save_direction
+    fruit = save_fruit
+    score = save_score
+
+
+
+def setup():
+    return pg.display.set_mode((600, 600)), pg.time.Clock()
+
+
+def handle_events():
+    global direction
+    global running
+    global bot
+    global score
     # on itère sur tous les évènements qui ont eu lieu depuis le précédent appel
     # ici donc tous les évènements survenus durant la seconde précédente
     for event in pg.event.get():
@@ -95,19 +119,25 @@ while running:
                 direction = [1, 0]
             if event.key == pg.K_b:
                 bot = not bot
+            if event.key == pg.K_s:
+                save_state(serpent, direction, fruit, score)
+            if event.key == pg.K_l:
+                load_state()
 
-    
+
+def move_snake():
+    global direction
+    global running
+    global bot
+    global score
+    # on vérifie si le bot doit être activé, si oui la direction est donnée à tout moment par dir_bot
     if bot:
         direction = dir_bot(serpent, direction, fruit)
-        
-
     tete = [serpent[-1][0] + direction[0], serpent[-1][1] + direction[1]]
-
     # autocollision
     if tete in serpent[:-1]:
         running = False
         print(f"Game over ! Self-bite \nScore : {score}")
-
     # traversée murs
     if tete[0] > 29:
         tete = [0, tete[1]]
@@ -117,32 +147,51 @@ while running:
         tete = [tete[0], 0]
     elif tete[1] < 0:
         tete = [tete[0], 29]
-
+    # on ajoute la tête au serpent
     serpent.append(tete)
-
+    # on lui retire la queue si il n'a pas mangé de fruit à cette frame
     if tete != fruit:
         serpent.pop(0)
+    # on dessine un nouveau fruit si le fruit a été mangé à cette frame
     else:
         fruit[0] = rd.randint(0, 29)
         fruit[1] = rd.randint(0, 29)
         score += 1
 
+
+def draw_frame(screen):
     # dessin du fond d'écran
     screen.fill((150, 200, 0))
     for pos in checker_index:
         case = pg.Rect(pos[0] * 20, pos[1] * 20, width, height)
         pg.draw.rect(screen, (200, 255, 0), case)
-
     # dessin du fruit
     rect_fruit = pg.Rect(fruit[0] * 20, fruit[1] * 20, width, height)
     pg.draw.rect(screen, color_fruit, rect_fruit)
-
     # dessin du serpent
     for pos in serpent:
         rect = pg.Rect(pos[0] * 20, pos[1] * 20, width, height)
         pg.draw.rect(screen, color_snake, rect)
-
+    #affichage du score
     pg.display.update()
     pg.display.set_caption(f"🐍 Score: {score}")
+
+
+def wait_for_next_frame(clock):
+    clock.tick(3)
+
+
+
+screen, clock = setup()
+
+while running:
+
+    handle_events()
+
+    move_snake()
+
+    draw_frame(screen)
+
+    wait_for_next_frame(clock)
 
 pg.quit()
